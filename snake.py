d5be87dc013e0264142908229e18ba4dffdf38b5
User <pereraniveka@gmail.com>
2025-07-15 23:13:36 +0530

    2nd commit

--- snake.py
+++ snake.py
@@ -9,13 +9,94 @@ TILE_SIZE =25
 WINDOW_WIDTH = TILE_SIZE * ROWS
 WINDOW_HEIGHT = TILE_SIZE * COLS
 
+
+class Tile:
+    def __init__(self, x, y):
+        self.x= x
+        self.y= y
+
 #game window
 window = tkinter.Tk()
 window.title("Snake")
 window.resizable(False, False) #cannot change window size
 
-canvas= tkinter.Canvas(window, bg="black", width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
+canvas= tkinter.Canvas(window, bg="black", width=WINDOW_WIDTH, height=WINDOW_HEIGHT, borderwidth=0, highlightthickness=0)
 canvas.pack()
 window.update()
 
+#center the window
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width= window.winfo_screenwidth()
+screen_height= window.winfo_screenheight()
+
+window_x= int((screen_width/2)-(window_width/2))
+window_y= int((screen_height/2)-(window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
+
+#initialize game
+snake= Tile(5*TILE_SIZE,5*TILE_SIZE) #single tile, snake's head
+food= Tile(10*TILE_SIZE, 10*TILE_SIZE)
+snake_body = []
+velocityX= 0
+velocityY =0
+
+def change_direction(e):
+    # print(e)
+    # print(e.keysym)
+    global velocityX, velocityY
+    if e.keysym== "Up" and velocityY != 1:
+        velocityX=0
+        velocityY= -1
+    elif e.keysym == "Down" and velocityY != -1:
+        velocityX=0
+        velocityY= 1
+    elif e.keysym== "Left" and velocityX != 1:
+        velocityX= -1
+        velocityY= 0
+    elif e.keysym == "Right" and velocityX != 1:
+        velocityX= 1
+        velocityY=0
+
+def move():
+    global snake
+
+    #collision
+    if snake.x == food.x and snake.y == food.y:
+        snake_body.append(Tile(food.x, food.y))
+        food.x = random.randint(0, COLS-1) * TILE_SIZE
+        food.y = random.randint(0,ROWS-1) * TILE_SIZE
+
+    #update snake body
+    for i  in range(len(snake_body)-1,-1,-1):
+        tile= snake_body[i]
+        if(i==0):
+            tile.x= snake.x
+            tile.y =snake.y
+
+
+    snake.x += velocityX * TILE_SIZE
+    snake.y +=velocityY * TILE_SIZE
+
+
+def draw():
+    global snake
+    move()
+    canvas.delete("all")
+
+    # draw food
+    canvas.create_rectangle(food.x, food.y, food.x + TILE_SIZE, food.y + TILE_SIZE, fill="red")
+
+    #draw snake
+    canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill="lime green")
+
+    for tile in snake_body:
+        canvas.create_rectangle(tile.x, tile.y, tile.x + TILE_SIZE, tile.y + TILE_SIZE, fill="lime green")
+
+    window.after(100, draw) #100ms= 1/10s , 10 frames per second
+
+draw()
+
+window.bind("<KeyRelease>", change_direction)
 window.mainloop() #going to keep window on
