06293e92da2bac28884e66e8d810e4d2b6a687c6
User <pereraniveka@gmail.com>
2025-07-15 22:36:55 +0530

    initial commit

---
+++ snake.py
@@ -0,0 +1,21 @@
+#creating the window
+import tkinter #GUI library
+import random
+
+ROWS= 25
+COLS= 25
+TILE_SIZE =25
+
+WINDOW_WIDTH = TILE_SIZE * ROWS
+WINDOW_HEIGHT = TILE_SIZE * COLS
+
+#game window
+window = tkinter.Tk()
+window.title("Snake")
+window.resizable(False, False) #cannot change window size
+
+canvas= tkinter.Canvas(window, bg="black", width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
+canvas.pack()
+window.update()
+
+window.mainloop() #going to keep window on
