78fdcd37dd0770881a077b273cdad702957192de
User <pereraniveka@gmail.com>
2025-07-22 06:56:54 +0530

    3rd commit

--- snake.py
+++ snake.py
@@ -41,11 +41,16 @@ food= Tile(10*TILE_SIZE, 10*TILE_SIZE)
 snake_body = []
 velocityX= 0
 velocityY =0
+game_over= False
+score = 0
+
 
 def change_direction(e):
     # print(e)
     # print(e.keysym)
-    global velocityX, velocityY
+    global velocityX, velocityY, game_over
+    if game_over:
+        return
     if e.keysym== "Up" and velocityY != 1:
         velocityX=0
         velocityY= -1
@@ -60,13 +65,25 @@ def change_direction(e):
         velocityY=0
 
 def move():
-    global snake
+    global snake, food, snake_body, game_over, score
+    if game_over:
+        return
+    if snake.x < 0 or snake.x >= WINDOW_WIDTH or snake.y< 0 or snake.y >= WINDOW_HEIGHT:
+        game_over= True
+        return
+    for tile in snake_body:
+        if snake.x == tile.x and snake.y == tile.y:
+            game_over = True
+            return
+
+
 
     #collision
     if snake.x == food.x and snake.y == food.y:
         snake_body.append(Tile(food.x, food.y))
         food.x = random.randint(0, COLS-1) * TILE_SIZE
         food.y = random.randint(0,ROWS-1) * TILE_SIZE
+        score +=1
 
     #update snake body
     for i  in range(len(snake_body)-1,-1,-1):
@@ -74,6 +91,11 @@ def move():
         if(i==0):
             tile.x= snake.x
             tile.y =snake.y
+        else:
+            prev_tile= snake_body[i-1]
+            tile.x= prev_tile.x
+            tile.y= prev_tile.y
+
 
 
     snake.x += velocityX * TILE_SIZE
@@ -81,7 +103,7 @@ def move():
 
 
 def draw():
-    global snake
+    global snake, food, snake_body, game_over, score
     move()
     canvas.delete("all")
 
@@ -94,9 +116,14 @@ def draw():
     for tile in snake_body:
         canvas.create_rectangle(tile.x, tile.y, tile.x + TILE_SIZE, tile.y + TILE_SIZE, fill="lime green")
 
-    window.after(100, draw) #100ms= 1/10s , 10 frames per second
+    if game_over:
+        canvas.create_text(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, font="Arial 20", text=f"Game Over: {score}", fill="white")
+    else:
+        canvas.create_text(30,20, font = "Arial 10", text= f"{score}")
+    window.after(115,draw) #100ms= 1/10s , 10 frames per second
 
 draw()
 
 window.bind("<KeyRelease>", change_direction)
 window.mainloop() #going to keep window on
+
